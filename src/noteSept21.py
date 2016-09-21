b40ec1f52fc9920960de1e17fb9a32544634a847
sushmarchandran <sushmarchandran@users.noreply.github.com>
2016-09-21 16:15:17 -0400

    Update noteSept21.py

--- src/noteSept21.py
+++ src/noteSept21.py
@@ -12,3 +12,12 @@ def aha_distance(i, r1, r2):
             for col, row1, row2 in zip(i.cols, r2, r1):
                 distance += col.dist(row1, row2)
         return math.sqrt(distance)
+        
+  
+    def row_distance(self, row1, row2) :
+        distance = 0
+        index = 0
+        for index in xrange(len(self.cols) - 1):
+            col = self.cols[index]
+            distance += (col.dist(row1[index], row2[index]))
+        return math.sqrt(distance)
